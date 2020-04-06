171eb6feb560d93b52d03be317108ef914ff7ccd
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-04-06 15:46:14 -0500

    Created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HOmePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HOmePage></HOmePage>
+    </div>
+  );
+}
+
+export default App;
