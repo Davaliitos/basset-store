cb856b2f9335f6accec1280760d2ea2ef132d6f4
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-04-06 21:09:37 -0500

    Move homepage to small components. Finished styling menu component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HOmePage from './homepage.component';
-
+import HOmePage from './pages/homepage.component'
 function App() {
   return (
     <div>
